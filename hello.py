2eec49999fbbc6c2e788558d55cd04b5922fa713
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-01-23 23:26:10 +0000

    7

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello!")
+def create_hello_string(name):
+    return "hello, " + name
+
+def test_create_hello_string():
+    assert create_hello_string("world") == "hello, world"
+
+if __name__ == "__main__":
+    test_create_hello_string()
+    print("pass.")
