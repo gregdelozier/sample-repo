f973677f0e96be3b809aebe39ca3e555d3c4e1e6
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-01-24 00:28:10 +0000

    Added exclamation mark

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def create_hello_string(name):
-    return "hello, " + name
+    return "hello, " + name + "!"
 
 def test_create_hello_string():
-    assert create_hello_string("world") == "hello, world"
+    assert create_hello_string("world") == "hello, world!"
 
 if __name__ == "__main__":
     test_create_hello_string()
