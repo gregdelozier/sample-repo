c75d5683ca63cff95aa9750caff8a642c7c54088
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-01-30 23:13:21 +0000

    Added initial web app

---
+++ bottle_app.py
@@ -0,0 +1,12 @@
+# A very simple Bottle Hello World app for you to get started with...
+import os
+from bottle import default_app, run, route
+
+@route('/')
+def hello_world():
+    return 'Hello from Bottle (sample-repo)!'
+
+if "PYTHONANYWHERE_DOMAIN" in os.environ:
+    application = default_app()
+else:
+    run(host='localhost', port=8080)
