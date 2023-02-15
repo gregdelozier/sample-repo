a1ad385a6aef50563045390db8f5e58516bec694
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-02-15 17:50:26 -0500

    random updares

--- sketch.js
+++ sketch.js
@@ -14,11 +14,11 @@ function setup() {
 
 //function to create a ball when called
 function ball(){
-  circle(ball_x, ball_y, 60)
+  circle(ball_x, ball_y, 10)
 }
 
 function draw() {
-    background(50);
+    background(200);
     
     //logs the x and y postion of the user's mouseclick
     console.log(ball_x, ball_y)
