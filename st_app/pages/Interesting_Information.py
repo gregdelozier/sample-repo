5f5f14ae847e6ca5ae5845881940b93d157d9945
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-02-21 11:07:52 -0500

    Added some markdown

--- st_app/pages/Interesting_Information.py
+++ st_app/pages/Interesting_Information.py
@@ -3,3 +3,15 @@ import streamlit as st
 st.write("Hi! How are you doing today? This is silly.")
 
 st.line_chart([1,2,3,4,50,90,900])
+
+st.markdown("""
+## Useful VSCode key stuff
+
+* Multi-select vertical cursor: Control-option-arrow
+* Multi-select mouse cursor: fn-option-click
+
+## Simulations
+
+n(remaining_fun) = n(current_fun) * 0.12
+
+""")
